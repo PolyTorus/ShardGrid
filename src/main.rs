7d532aadee9f58bd33f9f22475ed00984adab5e6
quantumshiro <hiro.1nakanishi@gmail.com>
2024-01-19 16:09:21 +0900

    add: reaad files in tmp dirs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // tmpファイル読み込み
+    let entries = fs::read_dir("tmp/").unwrap();
+
+    // ファイル名とサイズを表示
+    for entry in entries {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let metadata = fs::metadata(&path).unwrap();
+
+        println!("{} is {} bytes", path.display(), metadata.len());
+    }
 }
